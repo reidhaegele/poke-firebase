5314d19d771e9672cae84e5b17a467ba51a0d1ea
Reid Haegele <haegelereid@gmail.com>
2024-02-25 23:32:23 -0600

    completed guessing game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokeService } from './poke.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,49 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'pokeguesser';
+export class AppComponent implements OnInit {
+  title = 'PokeGuesser';
+  poke = "";
+  sprite = null;
+  guess = "";
+  guessed = false;
+  correct = false;
+
+  @ViewChild("myinput") myInputField!: ElementRef;
+  ngAfterViewInit() {
+    this.myInputField.nativeElement.focus();
+  }
+
+  constructor(private pokeService: PokeService) {}
+
+  fetchPoke(): void {
+    this.guess = ""
+    this.guessed = false
+    this.correct = false
+    this.pokeService.getPoke().subscribe((data: any) => {
+      this.poke = data.name;
+      this.sprite = data.sprites.front_default
+    });
+    this.myInputField.nativeElement.focus();
+  }
+
+  onKey(event: any) {this.guess = event.target.value;}
+
+  async guessPoke(poke: string) {
+    try {
+      this.guessed = true
+      if (this.guess.toLowerCase() === poke) {
+        this.correct = true
+      }
+      else {
+        this.correct = false
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  ngOnInit(): void {
+    this.fetchPoke()
+  }
 }
---
+++ src/app/poke.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class PokeService {
+  url = 'https://pokeapi.co/api/v2/pokemon/';
+
+  constructor(private http: HttpClient) {}
+
+  getPoke(): Observable<any> {
+    return this.http.get(this.url + Math.floor(Math.random() * 151), { headers: { Accept: 'application/json' } });
+  }
+}
